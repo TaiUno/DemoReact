fd50681dbf61d95a722d75f476109e30e1310908
TaiUno <phamthanhtai236@gmail.com>
2019-08-14 20:53:19 +0700

    ver 1.0

--- src/App.js
+++ src/App.js
@@ -3,27 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 import Slideshow from './components/slideshow/slideshow';
-import img1 from './images/01.png';
-import img2 from './images/02.png';
-import img3 from './images/03.png';
-import img4 from './images/04.png';
+import img1 from './images/01.jpg';
+import img2 from './images/02.jpg';
+import img3 from './images/03.jpg';
+import img4 from './images/04.jpg';
+import img5 from './images/05.jpg';
 
 const collection = [
   { src: img1, caption: "Caption one" },
   { src: img2, caption: "Caption two" },
   { src: img3, caption: "Caption three" },
   { src: img4, caption: "Caption four" },
+  { src: img5, caption: "Caption five" },
 ];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Slideshow
+        {/* <Slideshow
           input={collection}
           ratio={`3:2`}
           mode={`manual`}
-        />
+        /> */}
 
         <Slideshow
           input={collection}
@@ -33,8 +35,7 @@ class App extends Component {
         />
 
         <div>
-          Made by <a href="https://about.phamvanlam.com/">Lam Pham</a>.
-          Visit me at <a href="/">completejavascript.com</a>.
+          Made by Tai Pham.
         </div>
       </div>
     );
